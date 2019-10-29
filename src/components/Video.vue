78e59d2ea12bb042c77345e644c6f3a1d0876d2e
platypusmt <54313903+platypusmt@users.noreply.github.com>
2019-10-29 09:44:35 -0300

    Add files via upload

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,13 +1,18 @@
 <template>
   <v-content>
     <v-container class="text-center">
-          <video  height="400" controls>
+          <video controls>
             <source src="../assets/video-zappa.mp4" type="video/mp4">
           </video>
     </v-container>
   </v-content>
 </template>
- 
+ <style>
+  video{
+    width: 100%;
+    min-width: 400px;
+  }
+ </style>
 <script>
 export default {
 }
--- src/views/Sobre.vue
+++ src/views/Sobre.vue
@@ -5,13 +5,13 @@
     <v-content  class="flex-row">
       <v-container>
         <v-row >
-          <v-col  cols="12"
+          <v-col
           md="6">
             <video  height="340" controls>
             <source src="../assets/video-zappa.mp4" type="video/mp4">
           </video>
           </v-col>
-          <v-col cols="12"
+          <v-col
           md="6">
             <div><h4>A escola de música” Zappa Maestro Instituto Musical” foi fundada no dia 13 de janeiro de 2014. Sendo idealizada em novembro de 2013, e fundada 2 meses em seguida.
               Após dois anos, Tiago Lucas e Saulo Diangelis, fizeram uma proposta de se associar a um professor que havia trabalhado com Tiago há alguns anos atrás, que acabou se juntando á eles dia 5 de dezembro de 2016. Completando no ano de 2019, dois anos de ’ Zappa Maestro Instituto Musical.’ Trabalhando em diversas áreas da música, ofertamos várias aulas de diferentes instrumentos. Tais como violão, viola caipira, guitarra, bateria e percussão, canto,técnica vocal, saxofone, teclado, piano, violino, flauta doce e transversal, clarinete, contrabaixo, ukulele.
